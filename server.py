345efafc4780f2447fe9acd9e24dc0bd1a89d122
RadostevAA <55791524+RadostevAA@users.noreply.github.com>
2019-12-26 22:34:03 +0300

    Готово
    Готово

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 sock = socket.socket()
 
@@ -8,24 +9,36 @@ try:
 except OSError:
     sock.bind(('', 8080))
     print("Using port 8080")
+while True:
+    sock.listen(5)
 
-sock.listen(5)
-
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
+    conn, addr = sock.accept()
+    print("Connected", addr)
+    
+    data = conn.recv(8192)
+    msg = data.decode()
+    
+    
+    msg1 = msg.split(' ')[1]
+    print(msg1[1:])
+    print(os.path.abspath('index.html'))
+    
+    if msg1 == '/':
+        file = open('index.html', 'r')
+        content = file.read()
+        conn.send(content.encode())
+        file.close()
+    elif os.path.exists(os.path.abspath(str(msg1[1:]))) == True:
+        file = open(str(msg1[1:]), 'r')
+        content = file.read()
+        conn.send(content.encode())
+        file.close()
+    else:
+        resp1 = """HTTP/1.1 404 NOT FOUND
+        Server: SelfMadeServer v0.0.1
+        Content-type: text/html
+        Connection: close
+        """
+        conn.send(resp1.encode())
 
 conn.close()
